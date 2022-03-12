58d1c4663411f7975ba05dc3ff0b94cee6dd1cfa
Jo Samuelsson <distress3@gmail.com>
2022-03-12 14:32:16 +0100

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let eachLetterArray = string.split('');
+    let backwards = eachLetterArray.reverse();
+    let backwardsString = backwards.join('');
+    return backwardsString
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
+//take a string as parameter
+//break string into array and store it in variable eachLetterArray
+    //.split()
+//reverse order of items in eachLetterArray with method .reverse()
+//make eachLetterArray into a string with .join('')
+//return eachLetterArray
