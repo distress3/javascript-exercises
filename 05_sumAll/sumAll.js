fccec9e934dfd00ead2b6379ba4798dbb1cb33dc
Jo Samuelsson <distress3@gmail.com>
2022-03-13 10:12:00 +0100

    solved 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
 const sumAll = function() {
-
+    let sum = 0;
+    if (typeof(arguments[0]) == 'number' && typeof(arguments[1]) == 'number') {
+        if (arguments[0] < 0 || arguments[1] < 0) {
+            return "ERROR";
+        }
+        if (arguments[0] < arguments[1]) {
+            for (let i = arguments[0]; i <= arguments[1]; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = arguments[1]; i <= arguments[0]; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
