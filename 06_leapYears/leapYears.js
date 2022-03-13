58b18f3307e5c1303887eef8f6f99d00f89d0ece
Jo Samuelsson <distress3@gmail.com>
2022-03-13 14:16:18 +0100

    solved 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,30 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) { //check if year is divisible by 4
+        if (year % 100 == 0) { //check if year is divisible by 100
+            if (year % 400 == 0) { //check if year is divisible by 400
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+    // switch (year) {
+    //     case (year % 4 != 0):
+    //         return false;
+    //     case (year % 4 == 0 && year % 100 != 0):
+    //         return true;
+    //     case (year % 100 == 0 && year % 400 != 0):
+    //         return false;
+    //     case (year % 100 == 0 && year % 400 == 0): 
+    //         return true;
+    // } 
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
