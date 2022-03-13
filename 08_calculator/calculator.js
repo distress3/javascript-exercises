82dafcdffa63c06b9779fc39cbf62ceace444430
Jo Samuelsson <distress3@gmail.com>
2022-03-13 17:55:36 +0100

    solved parts of 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,36 @@
 const add = function() {
-	
+	let sum = 0 + arguments[0] + arguments[1];
+  return sum;
 };
 
 const subtract = function() {
-	
+	let diff = 0;
+  if (arguments[0] > arguments[1]) {
+    diff = arguments[0] - arguments[1];
+  } else {
+    diff = arguments[1] - arguments[0];
+  }
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summed = 0;
+  for (let i = 0; i <= array.length; i++) {
+    if (typeof(array[i]) == 'number') {
+      summed += array[i];
+    }
+  }
+  return summed;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for (let i = 0; i <= array.length; i++) {
+    if (typeof(array[i]) == 'number') {
+      product *= array[i];
+    }
+  }
+  return product;
 };
 
 const power = function() {
