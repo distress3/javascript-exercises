565b985c6901bed88f37b229c9183a33af6e6cb5
Jo Samuelsson <distress3@gmail.com>
2022-03-13 14:52:05 +0100

    solved 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celsius = (temp - 32) / 1.8;
+  let celsRounded = Math.round(celsius * 10) / 10;
+  return celsRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenheit = (temp * 1.8) + 32 ;
+  let fahrRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrRounded;
 };
 
 // Do not edit below this line
