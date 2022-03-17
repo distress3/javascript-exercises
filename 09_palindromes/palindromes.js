b23d486d45542eace44feb8d9940f8852112c0c5
Jo Samuelsson <distress3@gmail.com>
2022-03-17 10:07:44 +0100

    solved 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // let punctuation = '!"#$%&\'()*+,-./:;<=>?@\ [\\]^_`{|}~';
+    let stringLowerCase = string.toLowerCase()
+    let punctuationLess = stringLowerCase.replace(/[.,\/#!$%\^&\*;:{}=\-_\ `~()]/g,"");
+    let stringArray = punctuationLess.split('')
+    // console.log(typeof stringArray);
+    let newArray = Array.prototype.slice.call(stringArray);
+    let reversedString = newArray.reverse()
+    console.log(stringArray, reversedString);
+    if (stringArray.toString() == reversedString.toString()) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+//doesnt actually reverse, probably because both reversedString
+//and stringArray are references to the same array.
+
+//fixed it to actually reverse but palindromes are still false?
+//maybe i need to make it a string again
+//removing spaces and making it to a string was correct.
 
 // Do not edit below this line
 module.exports = palindromes;
