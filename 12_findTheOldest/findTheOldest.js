0a07ef2615765462cbf9fed1afa5878b567126b4
Jo Samuelsson <distress3@gmail.com>
2022-03-17 21:29:28 +0100

    solved 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(array){
+    const date = new Date();
+    array.forEach(element => {
+        if (!element.yearOfDeath){
+            element.yearOfDeath = date.getFullYear();
+        }
+        element.age = checkAge(element.yearOfBirth, element.yearOfDeath);
+    });
+    let sortedArray = array.sort((c1, c2) => (c1.age < c2.age) ? 1 : (c1.age > c2.age) ? -1 : 0);
+    console.log(sortedArray[0])
+    return sortedArray[0];
+};
 
+function checkAge (yearOfBirth, yearOfDeath){
+    return age = yearOfDeath - yearOfBirth;
 };
 
 // Do not edit below this line
